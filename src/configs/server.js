ca4e1b2f55477cd9a66cb88488ade40177c778c4
unknown <martinvali12@gmail.com>
2022-07-07 20:05:46 +0300

    Add express-sslify package to enforce HTTPS connection

--- src/configs/server.js
+++ src/configs/server.js
@@ -4,11 +4,13 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server, { cors: "*" });
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const enforce = require("express-sslify");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/../views");
 app.use(express.static(__dirname + "/../public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 module.exports = { app, server, io };
