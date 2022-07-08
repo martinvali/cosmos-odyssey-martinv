53460b829135a9b56ea1bb46f3164204ce2c3f6f
unknown <martinvali12@gmail.com>
2022-07-08 18:22:11 +0300

    Make elements on askname page tabbable

--- src/public/askname/js/script.js
+++ src/public/askname/js/script.js
@@ -2,9 +2,15 @@ document
   .querySelector(".contact-form")
   .addEventListener("submit", function (e) {
     e.preventDefault();
-    const t = document.querySelector(".first-name-input").value,
-      n = document.querySelector(".surname-input").value;
-    (document.cookie = `firstName=${t}; Secure;`),
-      (document.cookie = `lastName=${n}; Secure;`),
+    const n = document.querySelector(".first-name-input").value,
+      t = document.querySelector(".surname-input").value;
+    (document.cookie = `firstname=${n}; Secure;`),
+      (document.cookie = `lastname=${t}; Secure;`),
       location.reload();
+  }),
+  window.addEventListener("keydown", function (e) {
+    9 === e.keyCode && document.body.classList.add("user-tabbing");
+  }),
+  window.addEventListener("mousedown", function () {
+    document.body.classList.remove("user-tabbing");
   });
