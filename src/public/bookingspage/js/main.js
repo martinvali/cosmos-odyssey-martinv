5922e8296aeb5a1c303e4d7e09bceebd003ab5d9
unknown <martinvali12@gmail.com>
2022-07-08 18:50:48 +0300

    Make bookingspage's elements tabbable

---
+++ src/public/bookingspage/js/main.js
@@ -0,0 +1,1 @@
+window.addEventListener("keydown",(function(e){9===e.keyCode&&document.body.classList.add("user-tabbing")})),window.addEventListener("mousedown",(function(){document.body.classList.remove("user-tabbing")}));
