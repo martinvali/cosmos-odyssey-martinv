ff4b86d17c9c9f7ec34c1f493164610583031d78
unknown <martinvali12@gmail.com>
2022-07-10 21:03:53 +0300

    Create MONGO_URI environment variable and configurations.env placeholder file

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,8 +2,6 @@ require("dotenv").config({ path: __dirname + "/configs.env" });
 
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.qdaga.mongodb.net/cosmos-odyssey?retryWrites=true&w=majority`
-);
+mongoose.connect(process.env.MONGO_URI);
 
 module.exports = mongoose;
